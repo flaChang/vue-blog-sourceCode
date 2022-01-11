76f6f8c0528392c80d2fd9bbf1fe8cfb94303fe0
fla's mac <zza46585@163.com>
2022-01-11 18:00:32 +0800

    创建并完善vue 路由

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+    |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view/>       <!--使用vue router的地方-->
 </template>
 
 <style>
---
+++ src/views/Login/template.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="login">
+    <h1>login</h1>
+  </div>
+</template>
+
+<script src="./template.js"></script>
+<style src="./template.css"></style>
+
